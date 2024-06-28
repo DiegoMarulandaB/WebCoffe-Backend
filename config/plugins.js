11e525ca04ae8616dcc7caee3b617f53514a5c61
Diego Marulanda B. <56690521+DiegoMarulandaB@users.noreply.github.com>
2024-06-27 21:04:17 -0500

    feat: cloudinary

--- config/plugins.js
+++ config/plugins.js
@@ -1,14 +1,19 @@
 // module.exports = () => ({});
-//backend rama feature
-module.exports = {
-  "strapi-neon-tech-db-branches": {
-    enabled: true,
+
+module.exports = ({ env }) => ({
+  upload: {
     config: {
-      neonApiKey:
-        "5ye7s17j5oh6pz3p40zzswjl2cuqaulbm4cgg405i05qmr8h9kinw94wpjvrissq", // get it from here: https://console.neon.tech/app/settings/api-keys
-      neonProjectName: "webCoffee", // the neon project under wich your DB runs
-      neonRole: "neondb_owner", // create it manually under roles for your project first
-      gitBranch: "main" // branch can be pinned via this config option. Will not use branch from git then. Usefull for preview/production deployment
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
     },
   },
-};
+});
